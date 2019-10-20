8c2c5ac0ed9dab98b2d325e7ed61ec445bda537c
kimbo <kimballleavitt@gmail.com>
2019-10-20 05:43:32 +0000

    remove userRouter import

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var rateLimit = require("express-rate-limit");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
